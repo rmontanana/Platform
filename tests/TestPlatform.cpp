cf83d1f8f4433f1e628b658acea63aa0b35e482d
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-04-03 20:51:21 +0200

    Add tests for libraries required versions

--- tests/TestPlatform.cpp
+++ tests/TestPlatform.cpp
@@ -6,11 +6,28 @@
 #include <map>
 #include <string>
 #include "TestUtils.h"
+#include "folding.hpp"
+#include <bayesnet/classifiers/TAN.h>
 #include "config.h"
 
 
-TEST_CASE("Test Python Classifiers score", "[PyClassifiers]")
+TEST_CASE("Test Platform version", "[Platform]")
 {
     std::string version = { platform_project_version.begin(), platform_project_version.end() };
     REQUIRE(version == "1.0.4");
+}
+TEST_CASE("Test Folding library version", "[Folding]")
+{
+    auto kfold = folding::KFold(5, 100);
+    REQUIRE(kfold.version() == "1.0.1");
+}
+TEST_CASE("Test BayesNet version", "[BayesNet]")
+{
+    std::string version = bayesnet::TAN().getVersion();
+    REQUIRE(version == "1.0.4");
+}
+TEST_CASE("Test mdlp version", "[mdlp]")
+{
+    std::string version = mdlp::CPPFImdlp::version();
+    REQUIRE(version == "1.1.2");
 }
